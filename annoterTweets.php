e80e77eedf07a252693e45a918abcd977720c047
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-11-23 12:31:46 +0100

    tools functions

--- annoterTweets.php
+++ annoterTweets.php
@@ -112,3 +112,27 @@ function getRandomAnnotedTweets($tweets,$nboftts=1,$counter_start=1)
 }
 
 
+
+// Check if one words is on string (array)
+function strposa($haystack, $needles=array(), $offset=0) {
+        $chr = array();
+        foreach($needles as $needle) {
+                $res = strpos($haystack, $needle, $offset);
+                if ($res !== false) $chr[$needle] = $res;
+        }
+        if(empty($chr)) return false;
+        return min($chr);
+}
+
+function getTweetIdByUrl($url)
+{
+	$arr = explode("/", $url);
+	return end($arr);
+}
+
+function getMinCountArrays($arrays)
+{
+	$counts = array_map('count', $arrays);
+	return min($counts);
+}
+
