965bb05ab9233bf7843e988911b1f801f8490715
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-12-20 15:21:48 +0100

    add predict script

---
+++ prediction/predict.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "START PREDCT SCRIPT"
+echo "DELETING OLD FILES"
+rm output/test.svm
+rm output/train.svm
+rm result/model.svm
+rm result/predict.svm
+rm result/result.txt
+
+echo "ADD LINE NUMBERS"
+php addLineNumber.php
+
+echo "RUN ANALYSER"
+php analyser.php
+
+
+echo "RUN PREDICTION"
+liblinear-2.21/train -c 4 -e 0.1 output/train.svm result/model
+liblinear-2.21/predict output/test.svm result/model result/predect
+
+
+echo "FORMAT PREDICTION"
+php predictFormater.php 
