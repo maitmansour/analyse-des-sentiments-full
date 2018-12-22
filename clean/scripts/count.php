543a94d8946368d202f9e88679a815746787cebd
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-12-22 11:56:36 +0100

    UP -  0.30603485003505 : add suicide

--- clean/scripts/count.php
+++ clean/scripts/count.php
@@ -3,5 +3,5 @@
 	$full_data=file_get_contents("../dataset/step1-clean-unlabeled-data.csv");
 
 foreach (array_count_values(str_word_count($full_data, 1)) as $key => $value) {
-    echo $key." ".$value.'<br>';
+    echo $value." ".$key.'<br>';
 }
